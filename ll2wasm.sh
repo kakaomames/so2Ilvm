d6cf54c27df082db07e174916a26f5544a657c48
kakaomame <nattyannkunndesu@icloud.com>
2026-08-30 23:12:16 +0900

    ll2wasm.sh の作成

---
+++ ll2wasm.sh
@@ -0,0 +1,10 @@
+# .ll をバイナリのビットコード (.bc) に変換
+llvm-as $1.so.ll -o $1.bc
+
+# .bc からオブジェクトファイルを作る (-O0)
+llc -O0 -march=wasm32 -filetype=obj $1.bc -o $1.o
+
+
+wasm-ld --no-entry --export-all -o $1.wasm $1.o
+
+
